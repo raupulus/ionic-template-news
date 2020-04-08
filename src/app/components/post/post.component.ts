dae983fbfe78479400a526a2f3a5528c9cdf78ab
Raúl Caro Pastorino <dev@fryntiz.es>
2020-04-08 23:46:48 +0200

    Preparo página para todas las categorías

---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/interfaces/interfaces';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: Post;
+  @Input() index: Number;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+
+  @ViewChild(IonSegment) segment: IonSegment;
+
+  categories = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'sience',
+    'sports',
+    'technology'
+  ]
 
   constructor() {}
+
+  ngOnInit() {
+    this.segment.value = this.categories[0];
+  }
 
 }
