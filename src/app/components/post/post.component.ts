78a1587e014d6db1e26f157fe92c1591ef4b2f94
Raúl Caro Pastorino <dev@fryntiz.es>
2020-04-13 01:00:06 +0200

    Implemento action sheet planteando acciones sobre los posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from 'src/app/interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-post',
@@ -12,12 +13,42 @@ export class PostComponent implements OnInit {
   @Input() post: Post;
   @Input() index: Number;
 
-  constructor( private iab: InAppBrowser ) { }
+  constructor( private iab: InAppBrowser,
+               private actionSheetCtrl: ActionSheetController ) { }
 
   ngOnInit() {}
 
   openPost() {
     // Indico que abra con el navegador de android usando el parámetro _system
     const browser = this.iab.create(this.post.url, '_system');
+  }
+
+  async openPostMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-sheet-dark',
+        handler: () => {
+          console.log('Compartir');
+        }
+      }, {
+        text: 'Añadir a Favorito',
+        icon: 'heart',
+        cssClass: 'action-sheet-dark',
+        handler: () => {
+          console.log('Favorito');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        cssClass: 'action-sheet-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancelar');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 }
